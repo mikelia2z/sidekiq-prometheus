54843011a28bd0f795fb5576ee9e1841a6b01b1f
Mike Li <lmike@amazon.com>
2025-04-24 12:51:42 -0700

    using mmap version prometheus client

--- lib/sidekiq_prometheus.rb
+++ lib/sidekiq_prometheus.rb
@@ -4,7 +4,7 @@ require "benchmark"
 require "rack"
 require "rackup"
 require "prometheus/client"
-require "prometheus/middleware/exporter"
+# require "prometheus/middleware/exporter"
 require "sidekiq"
 require "sidekiq/api"
 require "webrick"
@@ -233,7 +233,7 @@ module SidekiqPrometheus
     @_metrics_server ||= Thread.new do
       Rackup::Handler::WEBrick.run(
         Rack::Builder.new {
-          use Prometheus::Middleware::Exporter, registry: SidekiqPrometheus.registry
+          use Prometheus::Client::Rack::Exporter, registry: SidekiqPrometheus.registry
           run ->(_) { [301, {"Location" => "/metrics"}, []] }
         },
         **opts
